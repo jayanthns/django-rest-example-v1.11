21b89edf4489a0f3177573556d7fdcb95d99e7ac
nsjayanth <jayanth.ns@avetoconsulting.com>
2017-12-16 18:22:04 +0530

    login/register api code

--- sample_proj/settings.py
+++ sample_proj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'usermgmt',
 ]
 
@@ -46,12 +47,15 @@ AUTH_USER_MODEL = 'usermgmt.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'sample_proj.urls'
 
@@ -125,3 +129,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    # 'DEFAULT_PARSER_CLASSES': (
+    #     'rest_framework.parsers.JSONParser',
+    # )
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
+}
--- usermgmt/apiv1/serializers.py
+++ usermgmt/apiv1/serializers.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from ..models import User
 
@@ -15,3 +17,61 @@ class UserCreateSerializer(serializers.ModelSerializer):
                     "write_only": True
                 }
             }
+            
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+            username = username,
+            email = email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(allow_blank=True, read_only=True)
+    username = serializers.CharField(required=False, allow_blank=True)
+    email = serializers.EmailField(label="Email address", required=False, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'token',
+        ]
+
+        extra_kwargs = {
+                "password": {
+                    "write_only": True
+                }
+            }
+
+    def validate(self, data):
+        user_obj = None
+        email = data.get("email", None)
+        username = data.get("username", None)
+        password = data["password"]
+
+        if not email and not username:
+            raise ValidationError("A username or email is required to login.")
+
+        user = User.objects.filter(
+                Q(email=email) |
+                Q(username=username)
+            ).distinct()
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("This username/email is not valid.")
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect credentials please try again.")
+
+        data["token"] = "SOME RANDOM TOKEN"
+        return data
